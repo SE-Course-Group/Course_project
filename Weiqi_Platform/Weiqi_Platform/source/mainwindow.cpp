f45938d7d5d4411b66e5155bfcc00e400eb4f75c
zhangyue2000 <Caesar8310@sjtu.edu.cn>
2020-10-02 09:47:27 +0800

    主界面与游戏界面的切换

--- Weiqi_Platform/Weiqi_Platform/source/mainwindow.cpp
+++ Weiqi_Platform/Weiqi_Platform/source/mainwindow.cpp
@@ -13,3 +13,13 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_playButton_clicked()    //关闭开始界面，打开游戏界面
+{
+    this->close();
+
+    dialog = new Dialog(this);
+
+    dialog->setModal(false);
+    dialog->show();
+}
