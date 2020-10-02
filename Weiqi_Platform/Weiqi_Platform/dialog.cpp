a72cec27fdfeb07cf8822e4942bb27d0ec6cf1c7
zhangyue2000 <Caesar8310@sjtu.edu.cn>
2020-10-02 14:43:07 +0800

    棋子实现

--- Weiqi_Platform/Weiqi_Platform/dialog.cpp
+++ Weiqi_Platform/Weiqi_Platform/dialog.cpp
@@ -1,11 +1,18 @@
 #include "dialog.h"
 #include "ui_dialog.h"
+#include <QPainter>
+#include <QPen>
+#include <QMoveEvent>
+#include "mainwindow.h"
 
 Dialog::Dialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Dialog)
 {
     ui->setupUi(this);
+
+    chessX = -1;
+    chessY = -1;
 }
 
 Dialog::~Dialog()
@@ -15,5 +22,50 @@ Dialog::~Dialog()
 
 void Dialog::on_backButton_clicked()
 {
-    this->show();
+    MainWindow *fater = new MainWindow();
+    this->close();
+    fater->show();
+}
+
+void Dialog::paintEvent(QPaintEvent *)
+{
+  paint=new QPainter;
+  paint->begin(this);                                          //主窗口
+
+  paint->setPen(QPen(Qt::black,2,Qt::SolidLine));            //钢笔工具：颜色，线号，实线
+
+  //画SIZE+1条横线
+  for(int i=0;i<SIZE+1;i++)
+  {
+      paint->drawLine(x,y+WIDTH*i,x+WIDTH*(SIZE),y+WIDTH*i); //画线函数：x1,y1,x2,y2:画从(x1,y1)到(x2,y2)的线
+  }
+
+  //画SIZE+1条竖线
+  for(int i=0;i<SIZE+1;i++)
+  {
+      paint->drawLine(x+WIDTH*i,y,x+WIDTH*i,y+WIDTH*(SIZE));
+  }
+
+  //在棋盘正中做一个黑棋子
+  if(chessX != -1 && chessY != -1)
+  {
+      paint->setBrush(QBrush(Qt::black,Qt::SolidPattern));//毛刷：颜色，实图案
+      paint->drawEllipse(chessX-15,chessY-15,WIDTH/2,WIDTH/2);//画椭圆：中心点X,Y,宽度，高度
+  }
+}
+
+void Dialog::mousePressEvent(QMouseEvent *event)
+{
+    //获取点击的坐标
+    int newX = event->x();
+    int newY = event->y();
+
+    if( newX >= x && newY <= x + 8*WIDTH && newY >= y && newY <= y+8*WIDTH)
+    {
+        chessX = newX;
+        chessY = newY;
+    }
+
+    update();
+
 }
--- Weiqi_Platform/Weiqi_Platform/dialog.h
+++ Weiqi_Platform/Weiqi_Platform/dialog.h
@@ -2,6 +2,7 @@
 #define DIALOG_H
 
 #include <QDialog>
+#include <QPainter>
 
 namespace Ui {
 class Dialog;
@@ -14,12 +15,22 @@ class Dialog : public QDialog
 public:
     explicit Dialog(QWidget *parent = nullptr);
     ~Dialog();
+    const int SIZE=8;
+    const int WIDTH=65;
+    const int x=20,y=20;
+
+    int chessX,chessY;
 
 private slots:
     void on_backButton_clicked();
 
+protected:
+    void paintEvent(QPaintEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+
 private:
     Ui::Dialog *ui;
+    QPainter *paint;
 };
 
 #endif // DIALOG_H
